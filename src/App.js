a294ccce675e0120972b5f7addf4fc9486dceda6
Panagiotis Petsas <panagiotis.petsas@gmail.com>
2017-11-16 21:56:55 +0100

    UI facelift, dark theme, typewriter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import Typist from 'react-typist';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header/Header';
 import ProjectPreviews from './ProjectPreview/ProjectPreviews';
+import Typewriter from './Typewriter/Typewriter';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
+        <Typewriter />
           <ProjectPreviews/>
          TODO: implement 🚧 ⛑
       </div>
--- src/ProjectPreview/ProjectPreview.js
+++ src/ProjectPreview/ProjectPreview.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import { Carousel } from 'react-responsive-carousel';
-// import Carousel from 'react-image-carousel';
 import './ProjectPreview.css';
 
 const Carousel = require('nuka-carousel');
@@ -51,7 +49,7 @@ class ProjectPreview extends Component {
     const { name, description } = this.props.project;
     return (
       <div className="project-preview-container">
-        <div className="project-preview-item"> {name} </div>
+        <div className="project-preview-item name"> {name} </div>
         {this.renderCarousel()}
         <div className="project-preview-item"> {description} </div>
       </div>
