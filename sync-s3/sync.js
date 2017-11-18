a49d7d6d49ec609a309cfdbe976232434a3d86b0
petsinho <panagiotis.petsas@gmail.com>
2017-11-18 20:47:18 +0100

    added sync-s3 with native nodejs aws client

---
+++ sync-s3/sync.js
@@ -0,0 +1,46 @@
+// import s3 from 's3';
+// import { accessKeyId, secretAccessKey, region } from '../secrets';
+const s3 = require('s3');
+const path = require('path');
+const { accessKeyId, secretAccessKey, region } = require('../secrets');
+
+const client = s3.createClient({
+  maxAsyncS3: 20,     // this is the default
+  s3RetryCount: 3,    // this is the default
+  s3RetryDelay: 1000, // this is the default
+  multipartUploadThreshold: 20971520, // this is the default (20 MB)
+  multipartUploadSize: 15728640, // this is the default (15 MB)
+  s3Options: {
+    accessKeyId,
+    secretAccessKey,
+    region,
+        // endpoint: 's3.yourdomain.com',
+        // sslEnabled: false
+        // any other options are passed to new AWS.S3()
+        // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
+  },
+});
+
+
+const params = {
+  localDir: path.resolve(__dirname, '../', 'build'),
+  deleteRemoved: false, // default false, whether to remove s3 objects
+                         // that have no corresponding local file.
+
+  s3Params: {
+    Bucket: 'vanaheim',
+  //  Prefix: 'some/remote/dir/',
+      // other options supported by putObject, except Body and ContentLength.
+      // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
+  },
+};
+const uploader = client.uploadDir(params);
+uploader.on('error', (err) => {
+  console.error('unable to sync:', err.stack);
+});
+uploader.on('progress', () => {
+  console.log('progress', uploader.progressAmount, uploader.progressTotal);
+});
+uploader.on('end', () => {
+  console.log('done uploading');
+});
